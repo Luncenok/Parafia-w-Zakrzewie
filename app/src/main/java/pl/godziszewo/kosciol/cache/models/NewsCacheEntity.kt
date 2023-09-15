26c46ba19a4e378e2aa694eb952cb334ba907832
luncenok <idziejczak3@gmail.com>
2023-09-15 18:55:08 +0200

    Update dependencies, fix database error

--- app/src/main/java/pl/godziszewo/kosciol/cache/models/NewsCacheEntity.kt
+++ app/src/main/java/pl/godziszewo/kosciol/cache/models/NewsCacheEntity.kt
@@ -2,7 +2,7 @@
  * *
  *  * Created by Mateusz Idziejczak on 05.03.2022
  *  * Copyright (c) 2023 . All rights reserved.
- *  * Last modified 6/2/23, 3:46 PM
+ *  * Last modified 9/15/23, 6:55 PM
  *
  */
 
@@ -14,10 +14,9 @@ import pl.godziszewo.kosciol.cache.utils.CacheConstants
 
 @Entity(tableName = CacheConstants.NEWS_TABLE_NAME)
 data class NewsCacheEntity(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
     val date: String, // niedziela, 12.02.2023
-    val short: String,
+    @PrimaryKey val short: String,
     val mainImg: String,
     val elements: String? = null,
     val images: String? = null,
